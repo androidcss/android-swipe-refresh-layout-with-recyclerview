988d673973daf5e44d402fd3303b437417f09aba
gururajkharvi <gpkprivate@gmail.com>
2016-05-20 20:54:19 +0530

    ...

---
+++ Android/SwipeRefreshExample/app/src/main/java/com/androidcss/swiperefreshexample/DataFish.java
@@ -0,0 +1,10 @@
+package com.androidcss.swiperefreshexample;
+
+public class DataFish {
+
+    public String fishImage;
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public int price;
+}
